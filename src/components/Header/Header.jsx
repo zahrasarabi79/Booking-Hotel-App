7aa6610f11724197d20266329ae28b4ada74df03
Zahra Sarabi <zahrasarabi1379@gmail.com>
2024-02-24 20:05:44 +0330

    header search item

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
-
+import React, {useState} from "react";
+import {MdLocationOn}from 'react-icons/md'
+import {HiCalendar, HiSearch} from "react-icons/hi";
 const Header = () => {
+    const [destination, setDestination] = useState()
   return (
-    <div> Header </div>
+    <div className="header">
+      <div className="headerSearch">
+          <div className="headerSearchItem">
+              <MdLocationOn className="headerIcon locationIcon"/>
+              <input
+                  value={destination}
+                  onChange={(e) => setDestination(e.target.value)}
+                  type="text"
+                  placeholder="where to go?"
+                  className="headerSearchInput"
+                  name="destination"
+                  id="destination"
+              />
+              <span className="seperator"></span>
+          </div>
+          <div className="headerSearchItem">
+              <HiCalendar className="headerIcon dateIcon"/>
+              <div  className="dateDropDown">
+                hhh
+              </div>
+
+          </div>
+          <div className="headerSearchItem">
+              <div id="optionDropDown">
+                  1 adult . 2children . 1 room
+              </div>
+              <span className="seperator"></span>
+          </div>
+          <div className="headerSearchItem">
+              <button className="headerSearchBtn" >
+                  <HiSearch className="headerIcon"/>
+              </button>
+          </div>
+      </div>
+    </div>
   )
 }
 
